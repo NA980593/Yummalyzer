7ab37244a3dc5fe1399c15323216ff2879f78f52
kush <kd979681@wcupa.edu>
2025-03-30 02:49:04 -0400

    updated logo asset

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -63,7 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Image.asset(
-            'assets/images/yummalyzer_logo.jpg',
+            'assets/images/yummalyzer_logo.png',
             fit: BoxFit.contain,
           ),
         ),
