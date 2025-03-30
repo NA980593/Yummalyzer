c5e3e225a28e83d721a6b9b6159c817c1a161cac
NA980593 <90323608+NA980593@users.noreply.github.com>
2025-03-29 22:10:54 -0400

    rename camera class

--- lib/camera_gallery_button.dart
+++ lib/camera_gallery_button.dart
@@ -3,7 +3,7 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
 class CameraGalleryButton extends StatefulWidget {
-  const CameraGalleryButton({Key? key}) : super(key: key);
+  const CameraGalleryButton({super.key});
 
   @override
   State<CameraGalleryButton> createState() => CamGalButState();
