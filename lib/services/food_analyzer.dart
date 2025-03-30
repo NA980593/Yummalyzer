54849b7a3e35e7a68da2b254d133ca5a1587cba0
kush <kd979681@wcupa.edu>
2025-03-30 09:53:42 -0400

    fixed gemini working now

--- lib/services/food_analyzer.dart
+++ lib/services/food_analyzer.dart
@@ -61,9 +61,11 @@ Future<String> callGemini(dynamic imageInput) async {
   final analyzer = FoodAnalyzer(
     apiKey: apiKey,
     systemInstruction: '''
-    give me estimated information about the food in this picture in the following parsable format. do not put a space after the colon, do not put quotes, just put the answer itself right after the colon, nothing more. 
-    PLEASE make sure the format is in key:value, no brackets, no commas, no quotes. Include in the notes section things such as possible allergens, if the item is either healthy, not healthy, or moderately healthy, if the item item is highly processed, 
-    moderatly processed, or lightly/not processed, if high sodium , if high sugar. If it is not healthy, suggest a similar but healthier alternative. You MUST include everything and not leave anything out no matter what, make notes NO MORE THAN 20 WORDS.
+    give me estimated information about the food in this picture in the following parsable format. 
+    do not put a space after the colon, do not put quotes, just put the answer itself right after the colon, nothing more. 
+    PLEASE make sure the format is in key:value, no brackets, no commas, no quotes. 
+    Include in the notes section things such as possible allergens, if the item is either healthy, not healthy, or moderately healthy, 
+    if the item item is highly, moderatly, lightly/not processed, if high sodium or sugar. MUST include everything dont leave anything out.
     For the serving size ONLY say the number of items or grams, IF it is grams put the "g" after the value. keep name simplified version ie "Donut".
 
     Name:ANSWER
